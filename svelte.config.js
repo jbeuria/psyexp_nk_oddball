ac776015cd41b9cc76bd3d088659a4c62d103668
Jyotiranjan Beuria <jyotiranjan.beuria@gmail.com>
2023-04-10 12:41:32 +0530

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-const dev = process.argv.includes('dev');
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
-export default {
-	kit: {
-	  adapter: adapter({
-		// default options are shown
-		out: 'docs',
-		pages: 'docs',
-		assets: 'docs',
-		precompress: true,
-		envPrefix: '',
-		polyfill: true,
-		fallback: 'index.html'
-	  }),
-	  paths: {
-		base: dev ? '' : '/psyexp_nk_oddball',
-	  }
-	}
-  };
+const config = {
+ // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+ // for more information about preprocessors
+ preprocess: vitePreprocess(),
+
+ kit: {
+  adapter: adapter()
+ }
+};
+
+export default config;
